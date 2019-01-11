f8fc91d615b98585eb63a5ae303e23412b17cff8
Leo Louis <leo.louis@yahoo.com>
2019-01-11 14:50:54 +0530

    Add files via upload

---
+++ Clearing Prefetch - Temp Folders.bat
@@ -0,0 +1,17 @@
+@echo off
+setlocal
+call :Clear_Folder %SystemRoot%\TEMP
+pushd C:\Users
+for /d %%k in (*) do if exist "%%k\AppData\Local\Temp" call :Clear_Folder "%%k\AppData\Local\Temp"
+for /d %%k in (*) do if exist "C:\Windows\Prefetch" call :Clear_Folder "C:\Windows\Prefetch"
+popd
+endlocal
+goto :EOF
+
+
+:Clear_Folder
+pushd "%~1"
+for /d %%i in (*) do rd /s /q "%%i"
+del /f /q *
+popd
+goto :EOF
